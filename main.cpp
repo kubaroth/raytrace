e31d7d534102128445edbd5147a10801b2b139e7
kuba <kuba456@gmail.com>
2020-01-04 20:41:45 -0800

    rendering gradient (cuda) with updated vec3 to __host__ __device__ To compile main.cpp with nvcc: nvcc -x cu -I. main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// /usr/local/cuda-10.0/bin/nvcc -x cu -I. main.cu
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,16 +9,31 @@
 
 using std::cout;
 using std::endl;
-void render(vec3 *fb, int max_x, int max_y){
 
-    for (int j = max_y-1; j >=0; j--){
-        for (int i =0; i < max_x; i++){
-            int pixel_index = j*max_x + i;
-            fb[pixel_index] = vec3(float(i) / max_x,
-                                   float(j) / max_y,
-                                   0.2);
-        }
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line){
+    if (result) {
+        std::cerr << "CUDA error = " <<static_cast<unsigned int>(result) << " at " << file << ":" << line << " '" <<func << "' \n";
+        //Make sure we call CUDA Device Resetbefore exiting
+        cudaDeviceReset();
+        exit(99);
     }
+}
+
+
+__global__ void render(vec3 *fb, int max_x, int max_y){
+
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i >= max_x) || (j>=max_y))
+        return;
+    int pixel_index = j * max_x + i;
+            
+    fb[pixel_index] = vec3(float(i) / max_x,
+                           float(j) / max_y,
+                           0.2);
+
 }
 
 int main (){
@@ -28,13 +44,20 @@ int main (){
 
     //allocate FB
     vec3 *fb;
-    fb = new vec3[fb_size];
+    checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+
+    int tx = 8;
+    int ty = 8;
 
     clock_t start, stop;
     start = clock();
 
     // Render
-    render(fb,nx,ny);
+    dim3 blocks(nx/tx+1, ny/ty+1);
+    dim3 threads(tx,ty);
+    render <<<blocks, threads>>>(fb,nx,ny);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
 
     stop = clock();
     double timer_seconds = ((double)(stop-start)) / CLOCKS_PER_SEC;
@@ -42,5 +65,6 @@ int main (){
 
     save_png(fb, nx, ny);
 
+    checkCudaErrors(cudaFree(fb));
     return 0;
 }
