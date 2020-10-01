98e3a7b887bc0a68852c8b0bdcea201b39373d3e
kubaroth <kubaroth@gmail.com>
2020-10-01 07:32:05 -0700

    - fixed build time link to tbb (RPATH) by including tbb header - this forces     CMake to include tbb library as NEEDED and does not require setting LD_LIBRARY_PATH - basic skeleton of setting up Embree scene

--- main.cpp
+++ main.cpp
@@ -12,10 +12,11 @@
 
 #include <embree3/rtcore.h>
 
+#include <tbb/tbb.h>
 
 using std::cout;
 using std::endl;
-
+// using namespace tbb;
 
 bool hit_sphere(const vec3& center, float radius, const ray& r){
     vec3 oc = r.origin() - center;
@@ -84,11 +85,7 @@ int main (){
     int ny = 250;
     int num_pixels = nx*ny;
     size_t fb_size = num_pixels * sizeof(vec3);
-
-
-    RTCDevice rtcNewDevice();
-
-    
+ 
     //allocate FB
     vec3 *fb;
     fb = new vec3[fb_size];
@@ -108,6 +105,46 @@ int main (){
            d_world
         );
 
+    ////////// - Embree
+
+    RTCDevice device = rtcNewDevice(NULL);
+    RTCScene scene = rtcNewScene(device);
+
+    RTCGeometry geom = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_TRIANGLE);
+
+
+    float* vertices = (float*) rtcSetNewGeometryBuffer(geom,
+                                                     RTC_BUFFER_TYPE_VERTEX,
+                                                     0,
+                                                     RTC_FORMAT_FLOAT3,
+                                                     3*sizeof(float),
+                                                     3);
+
+    unsigned* indices = (unsigned*) rtcSetNewGeometryBuffer(geom,
+                                                          RTC_BUFFER_TYPE_INDEX,
+                                                          0,
+                                                          RTC_FORMAT_UINT3,
+                                                          3*sizeof(unsigned),
+                                                          1);
+
+    if (vertices && indices) {
+        vertices[0] = 0.f; vertices[1] = 0.f; vertices[2] = 0.f;
+        vertices[3] = 1.f; vertices[4] = 0.f; vertices[5] = 0.f;
+        vertices[6] = 0.f; vertices[7] = 1.f; vertices[8] = 0.f;
+
+        indices[0] = 0; indices[1] = 1; indices[2] = 2;
+    }
+
+    rtcCommitGeometry(geom);
+    rtcAttachGeometry(scene, geom);
+    rtcReleaseGeometry(geom);
+    rtcCommitScene(scene);
+
+    rtcReleaseScene(scene);
+    rtcReleaseDevice(device);
+
+
+    //////////
     stop = clock();
     double timer_seconds = ((double)(stop-start)) / CLOCKS_PER_SEC;
     std::cerr << "elapsed time:" << timer_seconds << std::endl;
